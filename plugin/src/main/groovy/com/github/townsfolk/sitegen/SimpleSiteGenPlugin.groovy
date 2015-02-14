986fdd66f9a38f52db91d7cb0b563f47f6292b6d
Eric Berry <elberry@gmail.com>
2015-02-14 14:50:56 -0800

    Initial Commit

---
+++ plugin/src/main/groovy/com/github/townsfolk/sitegen/SimpleSiteGenPlugin.groovy
@@ -0,0 +1,9 @@
+package com.github.townsfolk.sitegen
+
+/**
+ * @author elberry
+ * Created: Sat Feb 14 14:46:26 PST 2015
+ */
+class SimpleSiteGenPlugin {
+
+}
---
+++ plugin/src/main/groovy/com/github/townsfolk/sitegen/SiteGenPlugin.groovy
@@ -0,0 +1,15 @@
+package com.github.townsfolk.sitegen
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * @author elberry
+ * Created: Sat Feb 14 14:46:07 PST 2015
+ */
+class SiteGenPlugin implements Plugin<Project> {
+	@Override
+	void apply(Project project) {
+
+	}
+}
