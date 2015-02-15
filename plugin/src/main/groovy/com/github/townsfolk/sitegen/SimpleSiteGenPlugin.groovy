4f1c87834654b8ce7edba381e2e64f2a3145bacb
Eric Berry <elberry@gmail.com>
2015-02-15 00:03:19 -0800

    Implementing 'init' task for the SimpleSiteGenPlugin

--- plugin/src/main/groovy/com/github/townsfolk/sitegen/SimpleSiteGenPlugin.groovy
+++ plugin/src/main/groovy/com/github/townsfolk/sitegen/SimpleSiteGenPlugin.groovy
@@ -1,9 +1,86 @@
 package com.github.townsfolk.sitegen
 
+import org.gradle.api.Project
+
 /**
  * @author elberry
  * Created: Sat Feb 14 14:46:26 PST 2015
  */
-class SimpleSiteGenPlugin {
+class SimpleSiteGenPlugin extends SiteGenPlugin {
 
+	@Override
+	void apply(Project project) {
+		super.apply(project)
+		project.apply(plugin: 'groovy')
+		project.sourceSets {
+			main {
+				groovy {
+					srcDirs = ['layouts', 'site']
+				}
+			}
+		}
+	}
+
+	@Override
+	Closure getDistTask() {
+		return {
+
+		}
+	}
+
+	@Override
+	Closure getInitTask() {
+		return {
+
+			outputs.files layoutsDir, siteDir
+
+			doLast {
+				project.logger.debug("Initializing Project")
+
+				File mainLayout = createLayout("main.gsp")
+				mainLayout.text ?: (mainLayout.text = '''
+				<html>
+					<head>
+					  <title><sitemesh:write property='title'/></title>
+					  <sitemesh:write property='head'/>
+					</head>
+					<body>
+					  <sitemesh:write property='body'/>
+					</body>
+				</html>'''.stripIndent())
+
+				File indexPage = createPage("index.gsp")
+				indexPage.text ?: (indexPage.text = '''
+				<html>
+					<head>
+					  <title>${title}</title>
+					  <meta name='description' content='${description}'>
+					</head>
+					<body>
+					  ${content}
+					</body>
+				</html>'''.stripIndent())
+
+				File mainModel = createPage("model.groovy")
+				mainModel.text ?: (mainModel.text = '''
+				[
+					title: "Hello World",
+					description: "A simple page",
+					content: "<p>Hello <strong>world</strong>!</p>"
+				]'''.stripIndent())
+			}
+		}
+	}
+
+	@Override
+	Closure getRenderTask() {
+		return {
+
+			inputs.files layoutsDir, siteDir
+			outputs.dir renderedDir
+
+			doLast {
+			}
+		}
+	}
 }
--- plugin/src/main/groovy/com/github/townsfolk/sitegen/SiteGenPlugin.groovy
+++ plugin/src/main/groovy/com/github/townsfolk/sitegen/SiteGenPlugin.groovy
@@ -1,5 +1,6 @@
 package com.github.townsfolk.sitegen
 
+import org.gradle.api.GradleException
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
@@ -7,9 +8,67 @@ import org.gradle.api.Project
  * @author elberry
  * Created: Sat Feb 14 14:46:07 PST 2015
  */
-class SiteGenPlugin implements Plugin<Project> {
+abstract class SiteGenPlugin implements Plugin<Project> {
+
+	protected Project project
+
 	@Override
 	void apply(Project project) {
+		this.project = project
+		project.task("dist", distTask)
+		project.task("init", initTask)
+		project.task("render", renderTask)
+	}
 
+	abstract Closure getDistTask()
+
+	abstract Closure getInitTask()
+
+	abstract Closure getRenderTask()
+
+	private File createDirectory(String relativePath) {
+		File dir = new File(relativePath)
+		dir.exists() ?: dir.mkdirs() ?: {
+			throw new GradleException("'$relativePath' directory didn't exist, and could not be created.")
+		}
+		return dir
+	}
+
+	private File createFile(File directory, String relativePath) {
+		File file = new File(directory, relativePath)
+		file.exists() ?: file.parentFile.mkdirs() ?: file.createNewFile() ?: {
+			throw new GradleException("'${file}' didn't exist, and could not be created.")
+		}
+		return file
+	}
+
+	/**
+	 * Creates a new layout file under the layouts directory.
+	 * @param relativePath A file name, or path/file.
+	 * @return Created File
+	 */
+	protected File createLayout(String relativePath) {
+		createFile(layoutsDir, relativePath)
+	}
+
+	/**
+	 * Creates a new page or file under the site directory.
+	 * @param relativePath A File name, or path/file.
+	 * @return Created File
+	 */
+	protected File createPage(String relativePath) {
+		createFile(siteDir, relativePath)
+	}
+
+	protected File getLayoutsDir() {
+		createDirectory("layouts")
+	}
+
+	protected File getRenderedDir() {
+		createDirectory("${project.buildDir}/rendered")
+	}
+
+	protected File getSiteDir() {
+		createDirectory("site")
 	}
 }
